95e077326070bddf79ff1912f1545396ff5ceb28
Daphne <121971548+daphnewan@users.noreply.github.com>
2023-12-19 11:39:02 -0800

    Collisions setup

--- Player.gd
+++ Player.gd
@@ -1,5 +1,6 @@
 extends Area2D
-@export var Speed = 100
+@export var Speed = 400
+@onready var enemy_hit = $"../EnemyHit"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,16 +10,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 	
 	if velocity.length() >0:
 		velocity = velocity.normalized() * Speed
 	position += velocity * delta 
+	
+func _on_body_entered(body):
+	hide()
+	enemy_hit.play()
+	
 	
